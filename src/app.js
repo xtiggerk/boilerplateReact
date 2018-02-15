cd8fb5c5267e589b9a4623b0f19926553e11c44e
rjusserand <rjusserand@SQLI50577-6.SQLI>
2018-02-15 16:18:51 +0100

    boilerplate-react-v1

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
+import 'normalize.css/normalize.css';
+import './styles/style.scss';
+
+ReactDOM.render(<p>New BoilerPlate</p>, document.getElementById('app'));
